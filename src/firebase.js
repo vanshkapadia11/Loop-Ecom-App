d7f29b04c3ad4915e62122d80a094784f1383369
Vansh Kapadia <144871405+vanshkapadia11@users.noreply.github.com>
2025-07-17 11:51:39 +0530

    Changed Some Thing!!

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrL9zL5E-FE9La08sALNltnba5GEZeVgE",
-  authDomain: "authproject-494d7.firebaseapp.com",
-  projectId: "authproject-494d7",
-  storageBucket: "authproject-494d7.appspot.com", // ✅ FIXED HERE
-  messagingSenderId: "425249780603",
-  appId: "1:425249780603:web:cbac34728927081eb383de",
-  measurementId: "G-LB4NYZV3ZQ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
